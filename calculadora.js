3671caa285a7b5dd1eb693c63b9044c5435835f8
malethik <quintiliani.d@gmail.com>
2024-02-23 09:09:32 +0100

    initial commit

---
+++ calculadora.js
@@ -0,0 +1,52 @@
+const display = document.getElementById("display");
+
+const bottons = document.querySelector("button");
+
+//diplay
+const toDisplay = (value) => {
+  display.value += value;
+};
+//limpia diplay
+const clearDisplay = () => {
+  display.value = "";
+};
+//preparar array
+const newExpression = (expression) => {
+  const regex = /(\d+(\.\d+)?)\s*([*/+-](\d+(\.\d+)?))+/g;
+  return expression.match(regex);
+};
+//calculate result spostarlo sopra calcolatrice
+const calcolateResult = () => {
+  try {
+    const expression = display.value;
+    const result = evaluateExpression(expression);
+    display.value = result;
+  } catch (error) {
+    display.value = "ERRORE";
+  }
+};
+//calcolatrice
+const calculate = (parts) => {
+  let result = parseFloat(parts[0]);
+  for (let i = 1; i < parts.length; i += 2) {
+    const operator = parts[i];
+    const operand = parseFloat(parts[i + 1]);
+    switch (operator) {
+      case "+":
+        result += operand;
+        break;
+      case "-":
+        result -= operand;
+        break;
+      case "*":
+        result *= operand;
+        break;
+      case "/":
+        result /= operand;
+        break;
+      default:
+        throw new Error("Operatore non valido");
+    }
+  }
+  return result;
+};
