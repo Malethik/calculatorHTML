6c5bcc562d3aef794af4738ee7cfc37f3e578be2
malethik <quintiliani.d@gmail.com>
2024-02-24 15:49:25 +0100

    updating first version

--- feature.js
+++ feature.js
@@ -1,1 +1,25 @@
-console.log('hola');
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.button');
+// const onClick = (event) => {
+//   let input = event.target.value;
+//   console.log(input);
+//   return input;
+// };
+
+buttons.forEach((buttoni) => {
+  buttoni.addEventListener('click', () => {
+    const pressed = buttoni.textContent;
+
+    console.log(pressed);
+
+    if (buttoni.textContent === 'C') {
+      display.textContent = '0';
+    }
+    if (buttoni.textContent)
+      if (display.textContent === '0') {
+        display.textContent = pressed;
+      } else {
+        display.textContent += pressed;
+      }
+  });
+});
